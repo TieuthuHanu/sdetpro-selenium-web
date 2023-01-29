3ade80308f354388e71a3f595b9f429942217d13
NHU VO <Nhu Vo>
2023-01-28 19:57:07 -0800

    Lesson 19, test Featured Product

---
+++ src/test/java/test/product/FeaturedProductTest.java
@@ -0,0 +1,32 @@
+package test.product;
+
+import driver.DriverFactory;
+import models.components.global.footer.CustomerServiceColumnComponent;
+import models.components.global.footer.FooterColumnComponent;
+import models.components.global.footer.InformationColumnComponent;
+import models.components.product.ProductItemComponent;
+import models.pages.HomePage;
+import org.openqa.selenium.WebDriver;
+import url.UrlsDemoWebShop;
+
+import java.util.List;
+
+public class FeaturedProductTest {
+
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.getChromeDriver();
+        testFeaturedProductHomePage(driver);
+        driver.quit();
+    }
+
+    private static void testFeaturedProductHomePage(WebDriver driver) {
+        driver.get(UrlsDemoWebShop.baseUrl);
+        HomePage homePage = new HomePage(driver);
+        List<ProductItemComponent> productItemComps = homePage.productGridComp().productItemComps();
+
+        productItemComps.forEach(productItemComp -> {
+            System.out.println(productItemComp.productTitleElem().getText());
+            System.out.println(productItemComp.productLinkElem().getAttribute("href"));
+        });
+    }
+}
